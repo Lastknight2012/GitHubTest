b43e63324a42edf871a6d013bf3be60f7ac50595
lastknight2012 <yangguangpizza@gmail.com>
2013-02-21 15:56:13 +0800

    ByeBye Commit...
    Signed-off-by: lastknight2012 <yangguangpizza@gmail.com>

---
+++ GitHubTest/src/me/hypertech/github/ByeBye.java
@@ -0,0 +1,12 @@
+package me.hypertech.github;
+
+public class ByeBye {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("ByeBye...");
+	}
+
+}
---
+++ GitHubTest/src/me/hypertech/github/Forsee.java
@@ -0,0 +1,13 @@
+package me.hypertech.github;
+
+public class Forsee {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
